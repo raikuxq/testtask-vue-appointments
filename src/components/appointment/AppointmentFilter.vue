af8992b5d570e22dd6d7f52451b26fb1224c4aa4
Alexey Stepanov <Raddosx@gmail.com>
2020-09-24 20:14:45 +0300

    Add Appointment Filter component

---
+++ src/components/appointment/AppointmentFilter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="appointment-filter">
+    <a-select
+      :default-value="activeType"
+      placeholder="Select type"
+      @change="updateFilter"
+    >
+      <a-select-option
+        v-for="opt in getFilterOptions"
+        :value="opt.value"
+        :key="opt.value"
+      >
+        {{ opt.text }}
+      </a-select-option>
+    </a-select>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapState } from "vuex";
+
+export default {
+  name: "AppointmentFilter",
+  computed: {
+    ...mapState("appointments", ["activeType"]),
+    ...mapGetters("appointments", ["getFilterOptions"])
+  },
+  methods: {
+    ...mapActions("appointments", ["setActiveFilterType"]),
+    updateFilter(type) {
+      this.setActiveFilterType(type);
+    }
+  }
+};
+</script>
