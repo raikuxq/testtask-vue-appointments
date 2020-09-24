4676be5e2e6a609d22042ec17d522dc9d32544e7
Alexey Stepanov <Raddosx@gmail.com>
2020-09-24 20:13:30 +0300

    Add store entry point and setup persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
+/**
+ * Vuex modules
+ */
+import appointments from "@/store/appointments";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    appointments
+  },
+  plugins: [
+    createPersistedState({
+      paths: ["appointments"]
+    })
+  ]
 });
