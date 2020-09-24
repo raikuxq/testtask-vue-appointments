29a06c2c85c9e557412897a3dad5387ab86fbcdb
Alexey Stepanov <Raddosx@gmail.com>
2020-09-24 20:15:41 +0300

    Add Page Error

---
+++ src/views/PageError.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page-error">
+    <h1>An error has occurred</h1>
+    <br />
+    <a-button
+        type="primary"
+        @click="goToHomepage"
+    >Go to Homepage
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageError",
+  methods: {
+    goToHomepage() {
+      this.$router.push({ name: "PageHome" });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.page-error {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 60px 20px;
+}
+</style>
