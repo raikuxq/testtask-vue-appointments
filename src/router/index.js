286cc70038190e110af36d885983915f1b3fb9df
Alexey Stepanov <Raddosx@gmail.com>
2020-09-24 20:16:31 +0300

    Add router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
+export const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "PageHome",
+    component: () => import("@/views/PageHome")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/create-appointment",
+    name: "PageCreateAppointment",
+    component: () => import("@/views/PageCreateAppointment")
+  },
+  {
+    path: "/update-appointment/:id",
+    name: "PageUpdateAppointment",
+    component: () => import("@/views/PageUpdateAppointment")
+  },
+  {
+    path: "*",
+    name: "PageError",
+    component: () => import("@/views/PageError")
   }
 ];
 
